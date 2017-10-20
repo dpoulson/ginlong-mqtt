6e9ed53643bb2cd5e5ada5e0b21753c11054e2ed
Darren Poulson <darren.poulson@gmail.com>
2017-10-20 16:59:54 +0100

    Many updates. So far temp, dc amps, and total kwh is definite. Still working on other values

--- ginlong-listen.py
+++ ginlong-listen.py
@@ -26,81 +26,116 @@ while True:
     print >>sys.stderr, 'waiting for a connection'
     conn,addr = sock.accept()
     try:
-        print >>sys.stderr, 'connection from', addr
-        while True:
+        #print >>sys.stderr, 'connection from', addr
+        # while True:
             rawdata = conn.recv(1000)                                 # Read in a chunk of data
-            print >>sys.stderr, 'received data', rawdata
             hexdata = binascii.hexlify(rawdata)                       # Convert to hex for easier processing
-            print >>sys.stderr, 'Hex data: ', hexdata
 
-            if(len(hexdata) != 270):
+            if(len(hexdata) == 270):
+                timestamp = (time.strftime("%F %H:%M"))		# get date time
+                print >>sys.stderr, 'Hex data: ', hexdata
                 msgs = []
-                serial = binascii.unhexlify(str(hexdata[42:62]))      # Serial number is used for MQTT path, allowing multiple inverters to connect to a single instance
+                serial = binascii.unhexlify(str(hexdata[70:102]))     # Serial number is used for MQTT path, allowing multiple inverters to connect to a single instance
                 print "Serial %s" % serial
+                print "Length %s" % len(hexdata)
                 mqtt_topic = ''.join([client_id, "/", serial, "/"])   # Create the topic base using the client_id and serial number
+                if __debug__:
+                    print >>sys.stderr, 'MQTT Topic: ', mqtt_topic
 
                 ##### Vpv1
-                vpv1 = float(int(hexdata[66:69],16)/10)
+                vpv1 = float(int(hexdata[104:108],16))/10
+                if __debug__:
+                    print >>sys.stderr, 'vpv1: ', vpv1
                 msgs.append((mqtt_topic + "Vpv1", vpv1, 0, False))
 
                 ##### Vpv2
-                vpv2 = float(int(hexdata[70:73],16)/10)
+                vpv2 = float(int(hexdata[108:112],16))/10
+                if __debug__:
+                    print >>sys.stderr, 'vpv2: ', vpv2
                 msgs.append((mqtt_topic + "Vpv2", vpv2, 0, False))
 
                 ##### Ipv1
-                ipv1 = float(int(hexdata[78:81],16)/10)
+                ipv1 = float(int(hexdata[112:116],16))/10
+                if __debug__:
+                    print >>sys.stderr, 'ipv1: ', ipv1
                 msgs.append((mqtt_topic + "Ipv1", ipv1, 0, False))
 
                 ##### Ipv2
-                ipv2 = float(int(hexdata[82:85],16)/10)
+                ipv2 = float(int(hexdata[116:120],16))/10
+                if __debug__:
+                    print >>sys.stderr, 'ipv2: ', ipv2
                 msgs.append((mqtt_topic + "Ipv2", ipv2, 0, False))
 
                 ##### Vac1
-                vac1 = float(int(hexdata[102:105],16)/10)
+                vac1 = float(int(hexdata[144:148],16))/10
+                if __debug__:
+                    print >>sys.stderr, 'vac1: ', vac1
                 msgs.append((mqtt_topic + "Vac1", vac1, 0, False))
 
                 ##### Vac2
-                vac2 = float(int(hexdata[106:109],16)/10)
-                msgs.append((mqtt_topic + "Vac2", vac2, 0, False))
+                #vac2 = float(int(hexdata[106:109],16))/10
+                #if __debug__:
+                #    print >>sys.stderr, 'vac2: ', vac2
+                #msgs.append((mqtt_topic + "Vac2", vac2, 0, False))
 
                 ##### Vac3
-                vac3 = float(int(hexdata[110:113],16)/10)
-                msgs.append((mqtt_topic + "Vac3", vac3, 0, False))
+                #vac3 = float(int(hexdata[110:113],16))/10
+                #if __debug__:
+                #    print >>sys.stderr, 'vac3: ', vac3
+                #msgs.append((mqtt_topic + "Vac3", vac3, 0, False))
 
                 ##### Iac1
-                iac1 = float(int(hexdata[90:93],16)/10)
+                iac1 = float(int(hexdata[120:124],16))/10
+                if __debug__:
+                    print >>sys.stderr, 'iac1: ', iac1
                 msgs.append((mqtt_topic + "Iac1", iac1, 0, False))
 
                 ##### Iac2
-                iac2 = float(int(hexdata[94:97],16)/10)
-                msgs.append((mqtt_topic + "Iac2", iac2, 0, False))
+                #iac2 = float(int(hexdata[124:128],16))/10
+                #if __debug__:
+                #    print >>sys.stderr, 'iac2: ', iac2
+                #msgs.append((mqtt_topic + "Iac2", iac2, 0, False))
 
                 ##### Iac3
-                iac3 = float(int(hexdata[98:101],16)/10)
-                msgs.append((mqtt_topic + "Iac3", iac3, 0, False))
+                #iac3 = float(int(hexdata[128:132],16))/10
+                #if __debug__:
+                #    print >>sys.stderr, 'iac3: ', iac3
+                #msgs.append((mqtt_topic + "Iac3", iac3, 0, False))
 
                 ##### Pac
-                pac = float(int(hexdata[118:121],16)/10)
+                pac = float(int(hexdata[140:144],16))/10
+                if __debug__:
+                    print >>sys.stderr, 'pac: ', pac
                 msgs.append((mqtt_topic + "Pac", pac, 0, False))
 
                 ##### Fac
-                fac = float(int(hexdata[114:117],16)/10)
+                fac = float(int(hexdata[148:152],16))/100
+                if __debug__:
+                    print >>sys.stderr, 'fac: ', fac
                 msgs.append((mqtt_topic + "Fac", fac, 0, False))
 
                 ##### Temp
-                temp = float(int(hexdata[62:65],16)/10)
+                temp = float(int(hexdata[102:104],16))/10
+                if __debug__:
+                    print >>sys.stderr, 'temp: ', temp
                 msgs.append((mqtt_topic + "Temp", temp, 0, False))
 
                 ##### kWh today
-                kwhtoday = float(int(hexdata[138:141],16)/10)
+                kwhtoday = float(int(hexdata[156:160],16))/100
+                if __debug__:
+                    print >>sys.stderr, 'kwhtoday: ', kwhtoday
                 msgs.append((mqtt_topic + "kwhtoday", kwhtoday, 0, False))
 
                 ##### kWh total
-                #kwhtotal = float(int(hexdata[70:73],16)/10)
-                #msgs.append((mqtt_topic + "kwhtotal", kwhtotal, 0, False))
+                kwhtotal = float(int(hexdata[164:168],16))/10
+                if __debug__:
+                    print >>sys.stderr, 'kwhtotal: ', kwhtotal
+                msgs.append((mqtt_topic + "kwhtotal", kwhtotal, 0, False))
 
-                publish.multiple(msgs, hostname="localhost")
+                #publish.multiple(msgs, hostname="localhost")
+                file = open("rawlog",'a')
+                file.write(timestamp + ' ' + hexdata + '\n')
+                file.close()
 
     finally:
-        conn.close()
-
+        print "Finally"
